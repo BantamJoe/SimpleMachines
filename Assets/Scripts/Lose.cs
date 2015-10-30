a1b22fe13cfb47d9881eaab62f6bf67c8e982882
Troy Shields <troy.leland.shields@gmail.com>
2015-10-29 23:51:02 -0700

    Player can lose game

---
+++ Assets/Scripts/Lose.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lose : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (transform.position.y < -10)
+        {
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, Screen.width, Screen.height), "You lose!");
+            Time.timeScale = 0;
+        }
+    }
+}
