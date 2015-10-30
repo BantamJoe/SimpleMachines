564f42c1e3048575c425ba84bbbf82846c8835b2
Troy Shields <troy.leland.shields@gmail.com>
2015-10-29 22:02:38 -0700

    Initial commit. A ball sits on a rail.

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Builder : MonoBehaviour {
+
+    public GameObject selected;
+    
+    private Vector4 builderColor;
+    private bool on = true;
+
+	// Use this for initialization
+	void Start () {
+        builderColor = GetComponent<Renderer>().material.color;
+
+        InvokeRepeating("blink", 0, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(!selected.activeInHierarchy)
+        { 
+            positionGameObject(selected);
+        }
+    }
+
+    private void positionGameObject(GameObject obj)
+    {
+        var pos = transform.position; // this is where the Cube will appear when it's instantiated
+        var rot = Quaternion.identity; // Quaternion.identity essentially means "no rotation"
+        selected = (GameObject)Instantiate(obj, pos, rot); // The Instantiate command takes a GameObject, a Vector3 for position and a Quaternion for rotation.
+    }
+
+    void blink()
+    {
+        if (on)
+        {
+            GetComponent<Renderer>().material.color = new Vector4(1, 1, 1, 0);
+            on = false;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = builderColor;
+            on = true;
+        }
+    }
+}
---
+++ Assets/Scripts/BuilderCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuilderCam : MonoBehaviour {
+
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        this.cam = GetComponent<Camera>();
+        cam.orthographic = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cam.enabled = true;
+	}
+}
